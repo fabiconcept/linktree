064224987996e0da96341fe56df74a2c41116d4b
Fabian Ajokubi <favourajokubi@gmail.com>
2023-07-07 01:37:44 +0100

    added react-icons & react-hot-toast packages

--- importnat/firebase.jsx
+++ importnat/firebase.jsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
