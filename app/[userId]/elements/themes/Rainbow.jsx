6f3506ef6c6afe5a8e2bb323f476c3fdb506bcbb
Fabian Ajokubi <favourajokubi@gmail.com>
2023-07-28 04:44:17 +0100

    made the ranbow 45deg and added an overlaying blur

--- app/[userId]/elements/themes/Rainbow.jsx
+++ app/[userId]/elements/themes/Rainbow.jsx
@@ -3,8 +3,8 @@ import "./style/rainbow.css";
 export default function Rainbow() {
     return (
         <>
-            <div className="wrapper fixed top-0 left-0 h-screen w-screen"></div>
-            <div className="fixed h-screen w-screen backdrop-blur-[5px] bg-white bg-opacity-25 z-10"></div>
+            <div className="wrapper fixed top-0 left-0 h-screen w-screen scale-[2] rotate-[45deg]"></div>
+            <div className="fixed h-screen w-screen backdrop-blur-[50px] bg-white bg-opacity-25 z-10"></div>
         </>
     );
 }
