256a4a08dc9284ccd098fdf8ad6d7f2502d10728
Fabian Ajokubi <favourajokubi@gmail.com>
2023-07-30 00:12:47 +0100

    added the final theme card (3D Blocks)

--- app/[userId]/elements/Button.jsx
+++ app/[userId]/elements/Button.jsx
@@ -8,15 +8,31 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import IconDiv from "./IconDiv";
+import "./style/3d.css";
+import { baseUrlIcons, getCompanyFromUrl } from "@/lib/BrandLinks";
 
 export default function Button({ url, content, userId }) {
     const [modifierClass, setModifierClass] = useState("");
     const [specialElements, setSpecialElements] = useState(null);
+    const [selectedTheme, setSelectedTheme]= useState('');
     const [btnType, setBtnType] = useState(0);
     const [btnShadowColor, setBtnShadowColor] = useState('');
     const [btnFontColor, setBtnFontColor] = useState('');
     const [btnColor, setBtnColor] = useState('');
+    const [accentColor, setAccentColor] = useState([]);
+    const [btnFontStyle, setBtnFontStyle] = useState(null);
     const router = useRouter();
+
+    function getRootNameFromUrl(url) {
+        try {
+            const urlObj = new URL(makeValidUrl(url));
+            const rootName = urlObj.hostname;
+            return rootName;
+        } catch (error) {
+            console.log(error.message, url);
+            throw new Error(error);
+        }
+    }
 
     const [modifierStyles, setModifierStyles] = useState({
         backgroundColor: "",
@@ -40,8 +56,9 @@ export default function Button({ url, content, userId }) {
                 if (!docSnapshot.exists()) {
                     return;
                 }
-                const { btnType, btnShadowColor, btnFontColor, btnColor } = docSnapshot.data();
+                const { btnType, btnShadowColor, btnFontColor, btnColor, selectedTheme } = docSnapshot.data();
                 setBtnColor(btnColor ? btnColor : "#fff");
+                setSelectedTheme(selectedTheme);
                 setBtnFontColor(btnFontColor ? btnFontColor: "#000");
                 setBtnShadowColor(btnShadowColor ? btnShadowColor : "#000");
                 setBtnType(btnType);
@@ -52,6 +69,61 @@ export default function Button({ url, content, userId }) {
     }, []);
 
     useEffect(() => {
+        if (selectedTheme === "3D Blocks") {
+            const rootName = getRootNameFromUrl(url);
+            setModifierClass(`
+                relative after:absolute after:h-2 after:w-[100.5%] after:bg-black bg-white
+                after:-bottom-2 after:left-[1px] after:skew-x-[57deg] after:ml-[2px]
+                before:absolute before:h-[107%] before:w-3 before:bg-[currentColor]
+                before:top-[1px] before:border-2 before:border-black before:-right-3 before:skew-y-[30deg]
+                before:grid before:grid-rows-2
+                border-2 border-black inset-2
+                ml-[-20px]
+                btn
+            `);
+            setSpecialElements(null);
+            setBtnFontStyle({
+                color: "#fff"
+            })
+            switch (String(getCompanyFromUrl(rootName)).toLocaleLowerCase()) {
+                case 'tiktok':
+                    setAccentColor(["#ff0050", "#00f2ea"]);
+                    break;
+                case 'audiomack':
+                    setAccentColor(["#ffa200", "#2a2a2a"]);
+                    break;
+                case 'twitter':
+                    setAccentColor(["#1DA1F2", "#657786"]);
+                    break;
+                case 'linkedin':
+                    setAccentColor(["#0077b5", "#0077b5"]);
+                    break;
+                case 'spotify':
+                    setAccentColor(["#1DB954", "#1DB954"]);
+                    break;
+                case 'youtube':
+                    setAccentColor(["#FF0000", "#FF0000"]);
+                    break;
+                case 'reddit':
+                    setAccentColor(["#ff4500", "#5f99cf"]);
+                    break;
+                case 'paypal':
+                    setAccentColor(["#003087", "#009cde"]);
+                    break;
+                case 'instagram':
+                    setAccentColor(["#E1306C", "#833AB4"]);
+                    break;
+                case 'facebook':
+                    setAccentColor(["#4267B2", "#898F9C"]);
+                    break;
+            
+                default:
+                    setAccentColor(["#191414", "#14171A"]);
+                    break;
+            }
+            return;
+        }
+
         switch (btnType) {
             case 0:
                 setModifierClass("");
@@ -151,9 +223,13 @@ export default function Button({ url, content, userId }) {
                 setSpecialElements(null);
                 break;
         }
-    }, [btnType]);
+    }, [btnType, selectedTheme]);
 
     useEffect(() => {
+        if (selectedTheme === "3D Blocks") {
+            return;
+        }
+
         function getShadow () {
             switch (btnType) {
                 case 6:
@@ -180,9 +256,13 @@ export default function Button({ url, content, userId }) {
             ...previewsStyles,
             boxShadow: shadowStyle,
         }));
-    }, [btnShadowColor, btnType]);
+    }, [btnShadowColor, btnType, selectedTheme]);
 
     useEffect(() => {
+        if (selectedTheme === "3D Blocks") {
+            return;
+        }
+
         function getBtnColor () {
             switch (btnType) {
                 case 12:
@@ -201,9 +281,13 @@ export default function Button({ url, content, userId }) {
             ...previewsStyles,
             backgroundColor: `${backgroundStyle}`,
         }));
-    }, [btnColor, btnType]);
+    }, [btnColor, btnType, selectedTheme]);
 
     useEffect(() => {
+        if (selectedTheme === "3D Blocks") {
+            return;
+        }
+
         function getBtnFontColor () {
             switch (btnType) {
                 case 12:
@@ -218,21 +302,30 @@ export default function Button({ url, content, userId }) {
 
         const fontColorStyle = getBtnFontColor();
 
-        setModifierStyles((previewsStyles) => ({
+        setBtnFontStyle((previewsStyles) => ({
             ...previewsStyles,
             color: `${fontColorStyle}`,
         }));
-    }, [btnFontColor, btnType]);
+    }, [btnFontColor, btnType, selectedTheme]);
+
+    useEffect(()=>{
+        if (accentColor.length > 0) {
+            setModifierStyles({
+                backgroundColor: `${accentColor[0]}`,
+                color: `${accentColor[1]}`
+            });
+        }
+    }, [accentColor]);
 
     return (
         <Link
             href={makeValidUrl(url)}
-            className={`${modifierClass} cursor-pointer flex gap-3 items-center hover:scale-[1.015] active:scale-95 md:w-[35rem] sm:w-[30] w-[20rem] min-h-10 py-3 px-3`}
+            className={`${modifierClass} cursor-pointer flex gap-3 items-center hover:scale-[1.025] active:scale-95 md:w-[35rem] sm:w-[30] w-[20rem] min-h-10 py-3 px-3`}
             style={modifierStyles}
         >
             {specialElements}
             <IconDiv url={url} />
-            <span className="font-semibold truncate max-w-[90%] flex-1">{content}</span>
+            <span className="font-semibold truncate max-w-[90%] flex-1" style={btnFontStyle}>{content}</span>
         </Link>
     );
 }
