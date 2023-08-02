ba5338dfae722e152dade7751e8d0a09179507f5
Fabian Ajokubi <favourajokubi@gmail.com>
2023-08-02 04:16:28 +0100

    Took off the opacity class from the new background

--- app/[userId]/elements/themes/Custom Backgrounds/Image.jsx
+++ app/[userId]/elements/themes/Custom Backgrounds/Image.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ImageBg({image}) {
     return (
         <div className="w-full h-full grid place-items-center">
-            <Image src={image} alt="profile pic" height={1000} width={1000} priority className="w-full object-cover min-h-full opacity-[0.8]" />
+            <Image src={image} alt="profile pic" height={1000} width={1000} priority className="w-full object-cover min-h-full" />
         </div>
     );
 }
