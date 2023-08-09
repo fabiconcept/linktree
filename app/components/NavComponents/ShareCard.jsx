2ac2a2162c8c1e2fe6c1bfe2572ba1d62160fe6d
Fabian Ajokubi <favourajokubi@gmail.com>
2023-08-09 16:20:47 +0100

    Installed Crypto-js for encrypting session cookies

--- app/components/NavComponents/ShareCard.jsx
+++ app/components/NavComponents/ShareCard.jsx
@@ -31,14 +31,20 @@ export default function ShareCard() {
                     <div className="cursor-pointer grid place-items-center h-md aspect-square rounded-lg active:border-black border border-transparent active:scale-90 hover:bg-black hover:bg-opacity-5" onClick={handleClose}><Image src={"https://linktree.sirv.com/Images/icons/svgexport-40.svg"} alt="x" width={15} height={15} /></div>
                 </div>
                 <p className="text-sm opacity-50 px-3">Get more visitors by sharing your Linktree everywhere.</p>
-                <div className="grid">
-                    {homePage.map((page) => (
+                <div className="grid my-5">
+                    {currentPage[currentPage.length-1].page === "home" && homePage.map((page) => (
                         <ShareLiElement nextPage={"nextPage"}>
                             <div className="flex-1 flex gap-3 items-center">
                                 <Image src={page.icon} alt="x" width={60} height={60} />
                                 <span className="font-semibold">{page.title}</span>
                             </div>
-                            <Image src={page.arrowIcon} className="-rotate-90" alt="x" width={15} height={15} />
+                            <Image 
+                                src={page.arrowIcon} 
+                                className={`${page.arrowIcon ==="https://linktree.sirv.com/Images/icons/arrow.svg" ? "-rotate-90": ""}`}
+                                alt="x" 
+                                width={15} 
+                                height={15} 
+                            />
                         </ShareLiElement>
                     ))}
                 </div>
