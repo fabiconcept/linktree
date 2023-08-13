95031c5be5253e9d4b9dd1361be0796cb0ac6d83
Fabian Ajokubi <favourajokubi@gmail.com>
2023-08-13 12:01:22 +0100

    Improved the Sizing

--- app/dashboard/general components/PickBrandModal.jsx
+++ app/dashboard/general components/PickBrandModal.jsx
@@ -9,7 +9,7 @@ export default function PickBrandModal({closeFunction}) {
     }
 
     return (
-        <div className="h-screen w-screen z-[99999999] fixed top-0 left-0 grid place-items-center">
+        <div className="h-screen w-screen z-[9999999999] fixed top-0 left-0 grid place-items-center">
             <div className="absolute top-0 left-0 h-full w-full bg-black backdrop-blur-[1px] bg-opacity-[0.25]" onClick={handlerClose}></div>
             <main className="bg-white relative z-10 shadow-xl w-[80vw] min-w-[20rem] flex flex-col rounded-3xl enter">
                 <div className="px-8 py-5 flex  w-full border-b font-semibold justify-between items-center">
@@ -17,13 +17,13 @@ export default function PickBrandModal({closeFunction}) {
                     <span className="text-base">Add to Linktree</span>
                     <div className="p-3 hover:bg-black hover:bg-opacity-[0.05] cursor-pointer rounded-lg active:scale-90" onClick={handlerClose}><FaX className="text-sm font-thin" /></div>
                 </div>
-                <div className="md:px-12 px-6 py-5 max-h-[73vh] overflow-y-auto">
+                <div className="md:px-12 px-3 py-5 sm:max-h-[73vh] max-h-[60vh] overflow-y-auto">
                     <section>
-                        <div className="grid mb-5 px-1">
+                        <div className="grid mb-5 px-2">
                             <span className="font-semibold">Share your content</span>
                             <span className="opacity-60 text-sm">Share content directly on your Linktree</span>
                         </div>
-                        <div className="flex flex-wrap sm:px-4 px-2 pb-4 gap-x-5 gap-y-2">
+                        <div className="flex flex-wrap sm:px-4 px-0 pb-4 gap-x-5 gap-y-2">
                             {AddContents.map((content, index) => (
                                 <BrandAdd
                                     btnData={{ itemTitle: `${capitalizeFirstLetter(content.title)} Link`, itemUrl: "", uniqueType: `${capitalizeFirstLetter(content.title)}` }}
