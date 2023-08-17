16936ae628798a952cd3e168bd7f133882786a81
Fabian Ajokubi <favourajokubi@gmail.com>
2023-08-17 01:54:32 +0100

    removed "next/router"

--- app/[userId]/components/ProfilePic.jsx
+++ app/[userId]/components/ProfilePic.jsx
@@ -3,7 +3,6 @@ import { fireApp } from "@/important/firebase";
 import { fetchUserData } from "@/lib/fetch data/fetchUserData";
 import { collection, doc, onSnapshot } from "firebase/firestore";
 import Image from "next/image";
-import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import { useEffect } from "react";
 import Head from "next/head";
@@ -14,7 +13,6 @@ export default function ProfilePic({userId}) {
     const [isElementVisible, setIsElementVisible] = useState(null);
     const [profileImageUrl, setProfileImageUrl] = useState("");
     const profilePicRef = useRef();
-    const router = useRouter();
 
     useEffect(() => {
         async function fetchProfilePicture() {
