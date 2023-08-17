bfc3d97a8206a22eb3b6401762d7bc7c5fc5a36b
Fabian Ajokubi <favourajokubi@gmail.com>
2023-08-17 03:21:26 +0100

    qrcode size control 003

--- app/components/NavComponents/elements/MyQrCode.jsx
+++ app/components/NavComponents/elements/MyQrCode.jsx
@@ -15,8 +15,8 @@ export default function MyQrCode({ url }) {
         downloadLink.click();
     };
     return (
-        <div className="my-2 max-w-full">
-            <div className='mx-auto w-fit max-w-[250px] relative grid place-items-center mb-2 select-none pointer-events-none' ref={qrCodeRef}>
+        <div className="my-2 w-full">
+            <div className='mx-auto w-full max-w-[250px] relative grid place-items-center mb-2 select-none pointer-events-none' ref={qrCodeRef}>
                 <QRCode value={url} size={180} />
                 <div className='absolute rounded-lg bg-white w-12 p-2 border grid place-items-center scale-90 select-none'>
                     <Image
@@ -30,7 +30,7 @@ export default function MyQrCode({ url }) {
             </div>
             <MLink myUrl={url} />
 
-            <div className="w-full flex justify-between items-center p-3 rounded-xl select-none hover:bg-black hover:bg-opacity-5 cursor-pointer active:scale-95" onClick={downloadAsJPEG}>
+            <div className="w-full max-w-[250px] flex justify-between items-center border p-3 rounded-xl select-none hover:bg-black hover:bg-opacity-5 cursor-pointer active:scale-95" onClick={downloadAsJPEG}>
                 <div className='grid gap-2'>
                     <span className='font-semibold'>Download JPG</span>
                     <span className='text-sm opacity-60'>High quality image</span>
