a170380ac9f0019554363457ddbe9b1a2096bb17
Fabian Ajokubi <favourajokubi@gmail.com>
2024-04-09 10:49:15 +0100

    Confirmation Email: Implemented sending a confirmation email to new users. ✅

--- lib/authentication/createAccount.jsx
+++ lib/authentication/createAccount.jsx
@@ -1,18 +1,16 @@
 import { fireApp } from "@/important/firebase";
 import { generateId, realEscapeString} from "../utilities";
-import { toast } from "react-hot-toast";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { generateSalt, hashPassword } from "./encryption";
 import { EmailJs } from "../EmailJs";
 import { welcomeEmail } from "../emailTemplate";
 import Error from "next/error";
 
-let generatedUserId = '';
 
-const createAccount = async (data) => {
+export const createAccount = async (data) => {
     const { email, username, password } = data;
     const userId = generateId();
-    generatedUserId = userId;
+    const generatedUserId = userId;
 
     try {
         const accountRef = collection(fireApp, "accounts");
@@ -59,39 +57,10 @@ const createAccount = async (data) => {
             selectedTheme: "Lake White",
         });
 
+        return generatedUserId;
+
     } catch (error) {
         console.error(error)
         throw new Error(error);
     }
-}
-
-export const createAccountHandler = (data) => {
-    const promise = createAccount(data);
-    let status
-    toast.promise(
-        promise,
-        {
-            loading: "Setting up your account.",
-            error: "Could't complete registration",
-            success: "Setup complete",
-        },
-        {
-            style: {
-                border: '1px solid #8129D9',
-                padding: '16px',
-                color: '#8129D9',
-            },
-            iconTheme: {
-                primary: '#8129D9',
-                secondary: '#FFFAEE',
-            },
-        }
-    ).then(()=>{
-        status = 200
-    }).catch(()=>{
-        status = 400
-    });
-
-
-    return {code: status, userId:generatedUserId};
 }
