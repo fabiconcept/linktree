d2ceb210b797934cad9f594f66b42a1bf83fcd6a
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-06-24 12:09:57 +0100

    Attempt 02 resolve conflit

--- app/(forget password pages)/reset-password/page.jsx
+++ app/(forget password pages)/reset-password/page.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 export default function ResetPasswordPage() {
     const router = useRouter();
     router.push("/login");
+    
     return (
         <div>page</div>
     )
