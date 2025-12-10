11c40ff0857a5f29d2e879425e5a4bc215ba073f
fabiconcept <favourajokubi@gmail.com>
2025-12-10 08:49:10 +0100

    Add Google site verification meta tag to RootLayout
    AGAIN

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './styles/fonts.css';
 import { Inter } from 'next/font/google';
 import NextTopLoader from "nextjs-toploader";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Link Tree',
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="eDdSbajfbiXqKmKgyUZEBOobGiP4VGg1x-EnrTHgG_E" />
+        <meta name="google-site-verification" content="uDJzp2l9Yhzie-f1ShUgIajhasXvs3_0rZoOqOG87Ig" />
       </head>
       <body className={inter.className}>
         <NextTopLoader color='#8129D9' />
